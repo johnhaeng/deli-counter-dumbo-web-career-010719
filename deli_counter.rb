f3102e682cdece7acfde5db68c48c44c75154b42
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-14 05:38:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+def line(deli)
+  new_deli_line = []
+  
+  if deli.size == 0
+    puts "The line is currently empty."
+  else
+    deli.each_with_index do |name, index|
+      new_deli_line.push("#{index+1}. #{name}")
+    end
+     puts "The line is currently: #{new_deli_line.join(' ')}"
+  end
+end
+
+def take_a_number(current_line, new_person)
+  current_line.push(new_person)
+  puts "Welcome, #{new_person}. You are number #{current_line.size} in line."
+end
+
+def now_serving(deli_line)
+  if deli_line.size == 0
+    puts "There is nobody waiting to be served!"
+  
+  else
+    puts "Currently serving #{deli_line[0]}."
+    deli_line.shift
+  end
+end
